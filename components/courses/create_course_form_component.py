ccedf9a03c602f2d4173ed1311af976bdf1a2709
Nik <gagyanai@mail.ru>
2025-06-03 16:27:08 +0300

    7.6 Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,21 +1,18 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
-
+from elements.input import Input
+from elements.textarea import Textarea
 
 class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.create_title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
-        self.create_estimated_time_input = (
-            page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
-        )
-        self.create_description_textarea = (
-            page.get_by_test_id('create-course-form-description-input').locator('textarea').first
-        )
-        self.create_max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.create_min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
+        self.create_title_input = Input(page,'create-course-form-title-input','Create title')
+        self.create_estimated_time_input = Input(page,'create-course-form-estimated-time-input', 'Create estimated time')
+        self.create_description_textarea = Textarea(page,'create-course-form-description-input','Create description')
+        self.create_max_score_input = Input(page,'create-course-form-max-score-input','Create max score')
+        self.create_min_score_input = Input(page,'create-course-form-min-score-input','Create min score')
 
     def fill(
             self,
@@ -43,17 +40,17 @@ class CreateCourseFormComponent(BaseComponent):
             max_score: str,
             min_score: str
     ):
-        expect(self.create_title_input).to_be_visible()
-        expect(self.create_title_input).to_have_value(title)
+        self.create_title_input.check_visible()
+        self.create_title_input.check_have_value(title)
 
-        expect(self.create_estimated_time_input).to_be_visible()
-        expect(self.create_estimated_time_input).to_have_value(estimated_time)
+        self.create_estimated_time_input.check_visible()
+        self.create_estimated_time_input.check_have_value(estimated_time)
 
-        expect(self.create_description_textarea).to_be_visible()
-        expect(self.create_description_textarea).to_have_value(description)
+        self.create_description_textarea.check_visible()
+        self.create_description_textarea.check_have_value(description)
 
-        expect(self.create_max_score_input).to_be_visible()
-        expect(self.create_max_score_input).to_have_value(max_score)
+        self.create_max_score_input.check_visible()
+        self.create_max_score_input.check_have_value(max_score)
 
-        expect(self.create_min_score_input).to_be_visible()
-        expect(self.create_min_score_input).to_have_value(min_score)
+        self.create_min_score_input.check_visible()
+        self.create_min_score_input.check_have_value(min_score)
