a34c4b7c28d3fcb1b072d6fd6915d254c45d1536
Nik <gagyanai@mail.ru>
2025-06-20 18:43:45 +0300

    Fix errors

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -12,21 +12,17 @@ from pages.courses.create_course_page import CreateCoursePage
 def login_page(chromium_page: Page)-> LoginPage:
     return LoginPage(page=chromium_page)
 
-
 @pytest.fixture     # Фикстура для инициализации RegistrationPage
 def registration_page(chromium_page: Page) -> RegistrationPage:
     return RegistrationPage(page=chromium_page)
-
 
 @pytest.fixture     # Фикстура для инициализации DashboardPage
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
-
 @pytest.fixture     # Фикстура для инициализации DashboardPage
 def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page_with_state)
-
 
 @pytest.fixture     # Фикстура для инициализации CoursesListPage
 def courses_list_page(chromium_page: Page) -> CoursesListPage:
@@ -36,11 +32,9 @@ def courses_list_page(chromium_page: Page) -> CoursesListPage:
 def courses_list_page_with_state(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
 
-
 @pytest.fixture
 def create_course_page(chromium_page: Page) -> CreateCoursePage:
     return CreateCoursePage(page=chromium_page)
-
 
 @pytest.fixture
 def create_course_page_with_state(chromium_page_with_state: Page) -> CreateCoursePage:
