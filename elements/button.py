75ad0f5b2c11c4dbea12edac366b5edb550029a7
Nik <gagyanai@mail.ru>
2025-06-22 15:44:11 +0300

    11.3 Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -1,5 +1,6 @@
 import allure
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Locator
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
@@ -12,6 +13,12 @@ class Button(BaseElement):
     def type_of(self) -> str:
         return "button"
 
+    def get_locator(self, nth: int = 0,  **kwargs) -> Locator:
+        return super().get_locator(nth, **kwargs).locator('button')
+
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(**kwargs)}//button'
+
     def check_enabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is enabled'
 
@@ -20,6 +27,8 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
 
@@ -27,4 +36,6 @@ class Button(BaseElement):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
         
